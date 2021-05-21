1218bb2715593b8d8c231e6554825c7099649597
Aman Shrivastava <77502312+aman34503@users.noreply.github.com>
2021-05-21 11:39:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+function sendMoney(){
+    var enterName = document.getElementById("enterName").value;
+    var enterAmount = parseInt(document.getElementById("enterAmount").value);
+ var enterSName = document.getElementById("enterSName").value;
+  var findSenderBankAccount = enterSName + "BankBalance";
+    var enterSAmount =parseInt(document.getElementById(findSenderBankAccount).innerHTML);
+    if (enterAmount > enterSAmount) {
+       alert("Insufficient Balance.")
+    }
+ else {
+       var findUserBankAccount = enterName + "BankBalance";
+      
+       var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+       var myAccountBalance = parseInt(document.getElementById(findSenderBankAccount).innerHTML) - enterAmount;
+       document.getElementById(findSenderBankAccount).innerHTML = myAccountBalance;
+       document.getElementById(findUserBankAccount).innerHTML = finalAmount;
+       alert(`Successful Transaction !!  
+       $${enterAmount} is sent to ${enterName}@email.com.`)
+ 
+       // transaction history 
+       var createPTag = document.createElement("li");
+       var textNode = document.createTextNode(`Rs ${enterAmount} is sent from the sender with Email-id ${enterSName}@email.com
+  to recepient with Email-id ${enterName}@email.com on ${Date()}.`);
+       createPTag.appendChild(textNode);
+       var element = document.getElementById("transaction-history-body");
+       element.insertBefore(createPTag, element.firstChild);
+    }
+
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/8.6.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/8.6.2/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
+ }
